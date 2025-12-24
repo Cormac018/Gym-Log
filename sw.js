af2e69f7f2597bf56264580679f675ef0ea6724a
Cormac018 <cormac.reader@gmail.com>
2025-12-24 16:04:30 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,6 +7,8 @@ const ASSETS = [
   "./app.js",
   "./workouts.js",
   "./manifest.webmanifest"
+  "./android-chrome-192x192.png",
+"./android-chrome-512x512.png",
 ];
 
 self.addEventListener("install", event => {
